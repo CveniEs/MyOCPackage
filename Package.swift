2cd84a09590b5b90283a15a309c8d0c68e5e34a7
cvenies <taoqingwei@haodf.com>
2020-09-04 10:33:52 +0800

    修改指向

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
         .target(
             name: "MyOCPackage",
             path: "MyOCPackage",
-            publicHeadersPath: "MyOCPackage"
+            cSettings: [.headerSearchPath("MyOCPackage")]
         )
     ]
 )
